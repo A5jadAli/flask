db7e40e657039af46fb8bfe343a1bb707b7cdec9
Asjad <aliasjid009@gmail.com>
2024-07-11 11:20:57 +0500

    Path parameters functioanlity added in dynamic_urls.py file

--- Part-2/dynamic_urls.py
+++ Part-2/dynamic_urls.py
@@ -3,12 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
-    return "Welcome to our Home Page!"
+def home():
+    return "<h1>Welcome to our Home Page!</h1>"
 
-@app.route('/welcome/Asjad')
-def welcome_asjad():
-    return "Welcome Asjad Ali!"
+# path parameter
+# here we are handling name dynamically
+@app.route('/welcome/<name>')
+def welcome(name):
+    return f"<h1>Hello {name.title()}, Welcome to our Webpage!</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True)
