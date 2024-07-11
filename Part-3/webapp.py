59544c1f3a787004ab48e010c98fde4d52d99f8f
Asjad <aliasjid009@gmail.com>
2024-07-11 15:29:47 +0500

    Render template functioanality added

--- Part-3/webapp.py
+++ Part-3/webapp.py
@@ -1,31 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Welcome to the Home Page!</h1>"
+    return render_template('home.html')
 
-# path parameter in the URL
-@app.route('/welcome/<name>')
-def welcome(name):
-    return f"<h1> Hi {name.title()}, Welcome to the Welcome Page!</h1>"
-
-# how to get numbers as path parameter
-@app.route('/square/<int:num>')
-def square(num):
-    return f"<h1> The square of {num} is {num**2}</h1>"
-
-# two path parameters
-@app.route('/rectangle/<int:length>/<int:breadth>')
-def rectangle(length, breadth):
-    return f"<h1> The area of rectangle with length {length} and breadth {breadth} is {length*breadth}</h1>"
-
-# this is our second endpoint
 @app.route('/about')
 def about():
-    return "<h1>Welcome to the About Page!</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) # here debug=True is used to run the server in debug mode and it will automatically restart the server when we make any changes in the code
+    app.run(debug=True)
