c76ca78a60fe440c4332818b99886d30374c38f2
Asjad <aliasjid009@gmail.com>
2024-07-11 11:39:49 +0500

    URL Redirection functionality added

--- Part-2/url_redirection.py
+++ Part-2/url_redirection.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,12 +6,22 @@ app = Flask(__name__)
 def home():
     return "<h1>Welcome to our Home Page!</h1>"
 
+@app.route('/pass')
+def passed():
+    return "<h1>Congratulation, you've passed.</h1>"
+
+@app.route('/fail')
+def failed():
+    return "<h1>Sorry, you've failed.</h1>"
+
 @app.route('/score/<string:name>/<int:marks>')
 def score(name, marks):
-    if marks < 0:
-        pass
-    elif marks < 40:
-        pass
+    if marks < 30:
+        # redirect to failed page
+        return redirect(url_for('failed'))
+    else:
+        # redirect to passed page
+        return redirect(url_for('passed'))
 
 if __name__ == "__main__":
     app.run(debug=True)
