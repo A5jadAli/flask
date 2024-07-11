22a1027026ad02787bf8f7ff5ffb8357a28b5a77
Asjad <aliasjid009@gmail.com>
2024-07-11 15:10:43 +0500

    Part 3 added

---
+++ Part-3/webapp.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "<h1>Welcome to the Home Page!</h1>"
+
+# path parameter in the URL
+@app.route('/welcome/<name>')
+def welcome(name):
+    return f"<h1> Hi {name.title()}, Welcome to the Welcome Page!</h1>"
+
+# how to get numbers as path parameter
+@app.route('/square/<int:num>')
+def square(num):
+    return f"<h1> The square of {num} is {num**2}</h1>"
+
+# two path parameters
+@app.route('/rectangle/<int:length>/<int:breadth>')
+def rectangle(length, breadth):
+    return f"<h1> The area of rectangle with length {length} and breadth {breadth} is {length*breadth}</h1>"
+
+# this is our second endpoint
+@app.route('/about')
+def about():
+    return "<h1>Welcome to the About Page!</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True) # here debug=True is used to run the server in debug mode and it will automatically restart the server when we make any changes in the code
