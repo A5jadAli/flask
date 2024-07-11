d27ec8f11ac35626edb84d7583adcdf751994d87
Asjad <aliasjid009@gmail.com>
2024-07-11 12:21:23 +0500

    url_building.py file added

---
+++ Part-2/url_building.py
@@ -0,0 +1,30 @@
+import time
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to our Home Page!</h1>"
+
+@app.route('/pass/<sname>/<int:smarks>')
+def passed(sname, smarks):
+    return f"<h1>Congratulation {sname.title()}, you've passed with {smarks} marks.</h1>"
+
+@app.route('/fail/<sname>/<int:smarks>')
+def failed(sname, smarks):
+    return f"<h1>Sorry{sname.title()}, you've failed with {smarks} marks.</h1>"
+
+@app.route('/score/<name>/<int:marks>')
+def score(name, marks):
+    if marks < 30:
+        time.sleep(2)
+        # redirect to failed page
+        return redirect(url_for('failed', sname=name, smarks=marks))
+    else:
+        time.sleep(2)
+        # redirect to passed page
+        return redirect(url_for('passed'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
