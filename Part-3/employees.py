73f6762d14daf48f6b1233eaa1d1662e80a06b0f
Asjad <aliasjid009@gmail.com>
2024-07-13 13:08:39 +0500

    employees data added

---
+++ Part-3/employees.py
@@ -0,0 +1,27 @@
+employees_data = {
+    1: {
+        "name": "Zain",
+        "age": 22,
+        "position": "CEO"
+    },
+    2: {
+        "name": "Asjad",
+        "age": 22,
+        "position": "CTO"
+    },
+    3: {
+        "name": "Kashaf",
+        "age": 23,
+        "position": "DBA"
+    },
+    4: {
+        "name": "Basra",
+        "age": 21,
+        "position": "Web Developer"
+    },
+    5: {
+        "name": "Muneeb",
+        "age": 22,
+        "position": "Flutter App Developer"
+    }
+}
--- Part-3/webapp.py
+++ Part-3/webapp.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template
+from employees import employees_data
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title="Home")
+    return render_template('home.html', 
+                           title="Home")
+
+@app.route('/employees')
+def employees():
+    return render_template('employees.html', 
+                           title="Employees",
+                           emps=employees_data)
 
 @app.route('/evaluate/<int:num>')
 def evaluate(num):
-    return render_template('evaluate.html', title="Evaluate", number=num)
+    return render_template('evaluate.html', 
+                           title="Evaluate", 
+                           number=num)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title="About")
+    return render_template('about.html', 
+                           title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
