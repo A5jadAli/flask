a415a41c2d51c3c006a9596451d1bfdf70014b41
Asjad <aliasjid009@gmail.com>
2024-08-07 11:58:28 +0500

    create operation added with sql alchemy

--- Part-5/db.py
+++ Part-5/db.py
@@ -7,12 +7,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class employees(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    name = db.column(db.String(30), nullable=False)
-    age = db.column(db.Integer, nullable=False)
-    email = db.column(db.String(50), nullable=False, unique=True)
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"Employee('{self.name}', '{self.age}', '{self.email}')"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# first of all create an application context
+# then push the database
+# then create the database using db.create_all()
+# then commit the database
