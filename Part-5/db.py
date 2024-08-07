7c98dae3a5649c8678482a4252ba4937df706e6c
Asjad <aliasjid009@gmail.com>
2024-08-07 11:23:25 +0500

    SQLAlchemy continued

--- Part-5/db.py
+++ Part-5/db.py
@@ -1,1 +1,18 @@
-## This is where we will start SQL Alchemy and create the database
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class employees(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    name = db.column(db.String(30), nullable=False)
+    age = db.column(db.Integer, nullable=False)
+    email = db.column(db.String(50), nullable=False, unique=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
