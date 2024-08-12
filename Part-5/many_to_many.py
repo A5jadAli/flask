453c4533593422bd32794a5ab28d6c55fc509108
Asjad <aliasjid009@gmail.com>
2024-08-12 11:50:04 +0500

    many to many relationship added

--- Part-5/many_to_many.py
+++ Part-5/many_to_many.py
@@ -7,11 +7,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+customer_product = db.Table(
+    'customer_product',
+    db.Column('customer_id', db.Integer, db.ForeignKey('customers.id')),
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'))
+)
+
 class Customer(db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
+    items = db.relationship("Product", backref="owners", secondary=customer_product)
 
     def __repr__(self):
         return f"Customer('{self.name}', '{self.email}')"
